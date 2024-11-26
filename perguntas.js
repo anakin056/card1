a0eb0b46269142e24e3e3eb2fc1b6a0866e0b0fa
Gabriel Nakamura <00001088050700sp@al.educacao.sp.gov.br>
2024-11-26 01:03:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Gastronomia',
+    'O que Frango frito?',
+    'Um frango q tá frito ué'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o Estado que fica Atibaia?',
+    'Estado de São Paulo'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o planeta vermelho?',
+    'Marte'
+)
+
+criaCartao(
+    'Quimica',
+    'Qual a formula da água?',
+    'è H2O'
+)
